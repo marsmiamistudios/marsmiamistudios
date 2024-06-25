353486c686c2baeada4ba70f86c061f6e6dfedab
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-06-25 03:20:36 -0400

    tstyles title

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
 
-        <h3 className="font-bold text-4xl lg:text-4xl tracking-tight mb-10">
+        <h3 className="font-bold text-3xl lg:text-5xl tracking-tight mb-10">
           Mars Miami Recording Studio
         </h3>
      
