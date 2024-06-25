cb9b6b317dd0c9e8dbdbb0ceb3a24977bd9466ce
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-06-25 02:03:09 -0400

    updated styles

--- components/FeaturesAccordion.tsx
+++ components/FeaturesAccordion.tsx
@@ -246,7 +246,7 @@ const FeaturesAccordion = () => {
     >
       <div className="px-8">
         <h2 className="font-extrabold text-4xl lg:text-6xl tracking-tight mb-12 md:mb-24">
-        Miami's 
+        Miami&apos;s 
           <span className="bg-neutral text-neutral-content px-2 md:px-4 ml-1 md:ml-1.5 leading-relaxed whitespace-nowrap">
           Diamond Studio
           </span>
