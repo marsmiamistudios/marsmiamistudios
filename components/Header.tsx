f503a9d1c45649220db44d1130e0ae433b81d820
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-07-15 00:07:58 -0400

    studios

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react";
 import type { JSX } from "react";
-import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import ButtonSignin from "./ButtonSignin";
@@ -32,13 +31,8 @@ const cta: JSX.Element = <ButtonSignin extraStyle="btn-primary" />;
 // A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
 // The header is responsive, and on mobile, the links are hidden behind a burger button.
 const Header = () => {
-  const searchParams = useSearchParams();
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  // setIsOpen(false) when the route changes (i.e: when the user clicks on a link on mobile)
-  useEffect(() => {
-    setIsOpen(false);
-  }, [searchParams]);
 
   return (
     <header className="bg-base-200">
