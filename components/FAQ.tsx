5db54755b309c84cf3c8421ed20e2b5f9ec5b1d1
Genesis Barrios <genesisbarriosdev@gmail.com>
2025-01-24 04:04:27 -0500

    pricing update

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -21,7 +21,7 @@ const faqList: FAQItemProps[] = [
     answer: (
       <p>
         {/* By heading over to the Book page, you can login with your Gmail and schedule an appointment. */}
-        Contact us by  <a href={`mailto:${config.mailgun.supportEmail}`} target="_blank"  className="link link-hover" aria-label="Contact Support">Email </a> 
+        Contact us by <a href={`mailto:${config.mailgun.supportEmail}`} target="_blank"  className="link link-hover" aria-label="Contact Support">Email </a> 
         or Call/Text: <a href="tel:3058578177" className="link link-hover">(305) 857-8177</a>
       </p>
     ),
